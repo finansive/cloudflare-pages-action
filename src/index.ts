a130f1d80d83926e2d795b32f0c2f058645ad36f
Piotr Staniów <staniowp@gmail.com>
2022-07-06 20:03:39 +0200

    feat: Handle branch deployments

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,7 @@ try {
   const projectName = getInput("projectName", { required: true });
   const directory = getInput("directory", { required: true });
   const gitHubToken = getInput("gitHubToken", { required: true });
+  const branch = getInput("branch", { required: false });
 
   const octokit = getOctokit(gitHubToken);
 
@@ -63,8 +64,12 @@ try {
     if ${accountId} {
       $ export CLOUDFLARE_ACCOUNT_ID="${accountId}"
     }
-  
-    $$ npx wrangler@2 pages publish "${directory}" --project-name="${projectName}"
+
+    if ${branch} {
+      $$ npx wrangler@2 pages publish "${directory}" --project-name="${projectName}" --branch="${branch}"
+    } else {
+      $$ npx wrangler@2 pages publish "${directory}" --project-name="${projectName}"
+    }
     `;
 
     const response = await fetch(
